5f9a1f5ceabc3fc6966801a5d4e761bebaf61802
Naynesh Rathod <nayneshrathod@gmail.com>
2020-05-18 14:01:53 +0530

    initial commit

---
+++ src/app/my-example.ts
@@ -0,0 +1,142 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: 'app-root',
+    template: ` <app-header></app-header>
+        <app-counter></app-counter>
+        <app-footer></app-footer>`,
+    styles: [ `
+    h1{color:red}`]
+})
+class HelloWorldComponent {
+
+}
+
+@Component({
+    selector: 'app-header',
+    template: `
+    <nav class="site-header sticky-top py-1">
+      <div class="container d-flex flex-column flex-md-row justify-content-between">
+        <a class="py-2" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+        </a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Product</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Enterprise</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Support</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Pricing</a>
+        <a class="py-2 d-none d-md-inline-block" href="#">Cart</a>
+      </div>
+    </nav>
+    `,
+    styles: [ `
+    h1{color:orange}`]
+})
+class HeaderComponent {
+
+}
+
+@Component({
+    selector: 'app-footer',
+    template: `
+    <footer class="container py-5">
+      <div class="row">
+        <div class="col-12 col-md">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+          <small class="d-block mb-3 text-muted">© 2017-2018</small>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Features</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Cool stuff</a></li>
+            <li><a class="text-muted" href="#">Random feature</a></li>
+            <li><a class="text-muted" href="#">Team feature</a></li>
+            <li><a class="text-muted" href="#">Stuff for developers</a></li>
+            <li><a class="text-muted" href="#">Another one</a></li>
+            <li><a class="text-muted" href="#">Last time</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Resource</a></li>
+            <li><a class="text-muted" href="#">Resource name</a></li>
+            <li><a class="text-muted" href="#">Another resource</a></li>
+            <li><a class="text-muted" href="#">Final resource</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Resources</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Business</a></li>
+            <li><a class="text-muted" href="#">Education</a></li>
+            <li><a class="text-muted" href="#">Government</a></li>
+            <li><a class="text-muted" href="#">Gaming</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>About</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Team</a></li>
+            <li><a class="text-muted" href="#">Locations</a></li>
+            <li><a class="text-muted" href="#">Privacy</a></li>
+            <li><a class="text-muted" href="#">Terms</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>`,
+    styles: [ `
+    h1{color:green}`]
+})
+class FooterComponent {
+
+}
+
+@Component({
+    selector: 'app-counter',
+    template: `<div >
+                <h1>Main FIle </h1><br>
+                this is a value <h1>{{count}} {{ans}}</h1>
+                <hr>
+                <button class="btn btn-primary" (click)="increment()"><i class="fa fa-plus"></i> Increment</button>
+                <button class="btn btn-danger" (click)="decrement()"><i class="fa fa-minus"></i> Decrement</button>
+                </div>`,
+    styles: [`h1{color:blue}
+            div{height:700px;}`]
+})
+class CounterComponent implements OnInit {
+    count: number;
+    ans: string;
+    constructor() {}
+    ngOnInit() {
+        this.count = 0;
+    }
+    increment(){
+        this.count++;
+        if (this.count >= 0){
+          this.ans = '';
+        }
+    }
+    decrement(){
+        this.count--;
+        if (this.count <= 0){
+          this.ans = ' | Please use  increment';
+          this.count = 0;
+        }
+    }
+}
+
+
+
+@NgModule({
+    imports: [ BrowserModule ],
+    declarations: [ HelloWorldComponent, HeaderComponent, CounterComponent, FooterComponent],
+    bootstrap:    [ HelloWorldComponent ]
+})
+
+class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
